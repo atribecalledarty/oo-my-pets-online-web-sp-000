ff28b1f76dd6d981b307b7ccbf3b232f7706459a
Arthur Song <arth3rs0ng@gmail.com>
2019-04-08 00:49:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,16 @@
 class Owner
   # code goes here
+  @@all = []
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
 end
